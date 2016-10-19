16c2a206c2bca00940b6f3214741816e8e50e248
Sergey Guznin <sergey.guznin@icloud.com>
2016-10-19 19:35:34 +0300

    Initial Commit

---
+++ Parser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Parser
+//
+//  Created by Sergey Guznin on 19.10.16.
+//  Copyright © 2016 Sergey Guznin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
